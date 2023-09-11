976fdaaf367c7b3557fb1e8206c1023a4e1f0230
Neema Nachok <neemanachok29@gmail.com>
2023-09-11 11:49:42 +0300

    submit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,22 @@
 class Song:
     pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song._update_class_variables(artist, genre)
+
+    @classmethod
+    def _update_class_variables(cls, artist, genre):
+        cls.count += 1
+        cls.genres.add(genre)
+        cls.artists.add(artist)
+
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
